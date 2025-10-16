1296677e7ac34e4c5588685f2d45a708a507083d
Dnthe-05 <dongocthe2005cm@gmail.com>
2025-10-16 13:37:28 +0700

    Add file Details

---
+++ Controllers/DetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BT_XL_IMG.Controllers
+{
+    public class DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
